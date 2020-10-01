44a15ef17f5f7b59e83eec6ddc8daf6872aa0e0d
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2020-09-30 21:16:50 -0300

    multiple subjects

--- study/sandbox/sandbox.py
+++ study/sandbox/sandbox.py
@@ -1,55 +1,7 @@
-# [['11:30', '12:00'], ['15:00', '16:00'], ['18:00', '18:30']]
 
-cal1 = [['9:00', '10:30'], ['12:00', '13:00'], ['16:00', '18:00']]
-ava1 = ['9:00', '20:00']
-cal2 = [['10:00', '11:30'], ['12:30', '14:30'], ['14:30', '15:00'], ['16:00', '17:00']]
-ava2 = ['10:00', '18:30']
-min_meeting_timespan = 30
-
-
-def _parse_time_string_to_int(time: str):
-    hours, minutes = time.split(':', 1)
-    if minutes != '00':
-        minutes = 100 / (60/int(minutes))
-
-    return int(hours + str(minutes))
-
-
-def _extract_common_time_constrains(time_contrains: list):
-    min_constrain = max_constrain = None
-    for time in time_contrains:
-        min_individual_constrain, max_individual_constrain = map(_parse_time_string_to_int, time)
-        if not min_constrain or min_individual_constrain > min_constrain:
-            min_constrain = min_individual_constrain
-
-        if not max_constrain or max_individual_constrain < max_constrain:
-            max_constrain = max_individual_constrain
-
-    return min_constrain, max_constrain
-
-
-def _parse_int_to_minutes(time: int):
+def main():
     pass
 
 
-def _generate_common_calendar(time_contrains: tuple,
-                              slot_in_min: int):
-
-    min_constrain, max_constrain = time_contrains
-    slot_hour_rate = 100 / (60 / slot_in_min)
-    return {time_slot: False for time_slot in range(min_constrain,
-                                                    max_constrain,
-                                                    slot_hour_rate)}
-
-
-# def _find_available_time_for_calendar(users_calendar: list(list(str)),
-#                                       time_contrains: tuple(int),
-#                                       slot_in_min: int):
-
-#     common_calendar = _generate_common_calendar(time_contrains, slot_in_min)
-#     for meeting in users_calendar:
-
-
-
-# def _match_available_time_within_constrains(time_contrains: list(list),
-#                                             schedule: )
+if __name__ == '__main__':
+    main()
---
+++ study/sandbox/todo.py
@@ -0,0 +1,36 @@
+"""
+Find minimum number of words in a sentence, given a  document, using white-space as separator for words and (!, ?, .)
+as the separator for sentence. There can be multiple white-spaces between two words.
+"""
+"""
+Given an integer, re-arrange the integer such that first and last digit will be at first and second, second
+and second last digit will be at third and fourth position from left of the re-arranges integer and so on.
+Example:
+Input -> 12345678, Output -> 18273645
+Input -> 1234567, Output -> 1726354
+"""
+"""
+Given an array with n integers, check if it could become non-decreasing by removing at most 1 element.
+Example:
+Input: [3, 2, 4, 3], Output: false
+Input: [1, 4, 2, 3], Output: true
+Input: [4, 3, 2], Output: false
+Input: [1, 2, 3], Output: true
+"""
+"""
+Determine whether a circular array with magic indexes contains a full circle. The array is circular in the sense that,
+the entry A[i] points to entry A[A[i]]. Full circle means it covers all the elements of the array.
+As a follow-up, I was asked to solve the problem in O(1) space.
+Example:
+Input: [2, 3, 1, 4, 0], Output: true
+Input: [2, 3, 2, 4, 9], Output: false
+"""
+"""
+3 - Technical screen with the team lead and team manager. Went through my resume and asked some manageri
+
+4 - Algorithms and approach to problems.
+
+5 - API design and structuring
+
+6 - Programming with online code editor.
+"""
