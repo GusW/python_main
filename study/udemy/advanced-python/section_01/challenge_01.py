4877b8b5d55dfde83d28a78a29cf066f1b590ef2
Gustavo <gustavo.watanabe@gmail.com>
2022-09-19 14:54:16 +0200

    WIP advanced

---
+++ study/udemy/advanced-python/section_01/challenge_01.py
@@ -0,0 +1,15 @@
+print(
+    """
+Hey friend!
+Let me help you figuring out a good name for your beer.
+Please respond the following questions with a single word:
+"""
+)
+
+resp1 = input("What is your zodiac?\n")
+resp2 = input("What is your favorite color?\n")
+
+print(f"Your beer name is:\nThe {resp2} {resp1}")
+
+if __name__ == "__main__":
+    pass
---
+++ study/udemy/advanced-python/section_01/string_session.py
@@ -0,0 +1,8 @@
+print(
+    f"""
+Your name is {input("Tell me your first name: ")} {input("Tell me your last name: ")}
+"""
+)
+
+if __name__ == "__main__":
+    pass
